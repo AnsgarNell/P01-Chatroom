ec50c9f44225f93a1330c09417184facf2e9daf5
Ansgar Nell <ansgar.nell@gmail.com>
2019-09-22 21:27:42 +0200

    Added missing members, constructors and getters and setters.

--- starter_code/src/main/java/edu/udacity/java/nano/chat/Message.java
+++ starter_code/src/main/java/edu/udacity/java/nano/chat/Message.java
@@ -4,6 +4,50 @@ package edu.udacity.java.nano.chat;
  * WebSocket message model
  */
 public class Message {
-    // TODO: add message model.
-    private String name;
+    private MessageType type;
+    private String username;
+    private String msg;
+    private int onlineCount;
+
+    public Message() {
+    }
+
+    public Message(MessageType type, String username, String msg, int onlineCount) {
+        this.type = type;
+        this.username = username;
+        this.msg = msg;
+        this.onlineCount = onlineCount;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public int getOnlineCount() {
+        return onlineCount;
+    }
+
+    public void setOnlineCount(int onlineCount) {
+        this.onlineCount = onlineCount;
+    }
 }
